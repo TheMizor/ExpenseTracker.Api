0d778d87869cca003a5afb241fe4a73206dfcfe5
TheMizor <simongirard702@gmail.com>
2026-06-08 15:22:34 +0200

    feat: ajout des entités et configuration d'EF Core avec SQLite

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using ExpenseTracker.Api.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
